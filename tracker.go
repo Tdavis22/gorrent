7424fb7859b5f5a772d90c20198b2e28e9dc3358
Ryan Kruger <rtkruger@calpoly.edu>
2018-12-16 09:39:28 -0800

    built the stuff

--- tracker.go
+++ tracker.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
+        "sync"
 )
 
 
 
 /* Waits for a get request, sees if the file exists, and returns a response accordingly. */
-func tracker(recv chan get_request, exit chan int, who_has map[string]file_info) {
+func tracker(recv chan get_request, exit chan int, who_has map[string]file_info,print_mutex *sync.Mutex) {
     ticker := time.NewTicker(5000 * time.Millisecond)
     for range ticker.C {
 	select {
@@ -18,7 +19,7 @@ func tracker(recv chan get_request, exit chan int, who_has map[string]file_info)
 	            req.port <- tracker_response{"", "success, file found", 5, 0, "*_*", 
                                                  who_has[req.info_hash].num_seeders, 
                                                  who_has[req.info_hash].num_leechers, 
-                                                 who_has[req.info_hash].peers}
+                                                 who_has[req.info_hash].peer_list}
                 } else {
                     req.port <- tracker_response{"File not found", "Request denied", 0,0,"",
                                                  0,0,nil}
