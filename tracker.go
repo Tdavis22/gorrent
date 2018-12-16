778681ac7af2684ec9a2dee63448995715e2ff92
anujkak <anujkak@users.noreply.github.com>
2018-12-16 08:31:18 -0800

    added basic tracker

--- tracker.go
+++ tracker.go
@@ -5,7 +5,6 @@ import (
 	"time"
 )
 
-
 /* Staged for deletion. Replaced by tracker2.0
 type peer struct {
 	id   string
@@ -16,7 +15,7 @@ type client_info struct {
 	t        int
 }
 func handle_request(req get_request, who_has map[string][]peer) {
-	req.port <- tracker_response{"success", "", 5, 0, "*_*", num_seeders, num_leechers, 
+	req.port <- tracker_response{"success", "", 5, 0, "*_*", num_seeders, num_leechers,
                                      who_has[req.info_hash]}
 }
 func tracker_server(recv chan get_request, exit chan int, peers map[string][]peer) {
@@ -39,30 +38,28 @@ func tracker_server(recv chan get_request, exit chan int, peers map[string][]pee
 }
 */
 
-
 /* Waits for a get request, sees if the file exists, and returns a response accordingly. */
-func tracker2.0(recv chan get_request, exit chan int, who_has map[string]file_info) {
-    ticker := time.NewTicker(5000 * time.Millisecond)
-    for range ticker.C {
-	select {
-	    case req := <-recv:
-                _,ok := who_has[req.info_hash]
-                if ok {
-	            req.port <- tracker_response{"", "success, file found", 5, 0, "*_*", 
-                                                 who_has[req.info_hash].num_seeders, 
-                                                 who_has[req.info_hash].num_leechers, 
-                                                 who_has[req.info_hash].peers}
-                } else {
-                    req.port <- tracker_response{"File not found", "Request denied", 0,0,"",
-                                                 0,0,nil}
-                }
-	    case <-exit:
-		fmt.Println("Tracker server closing.")
-		return
-	    default:
-	        fmt.Println("Tracker server standby.")
-        }
-    }
+func tracker(recv chan get_request, exit chan int, who_has map[string]file_info) {
+	ticker := time.NewTicker(5000 * time.Millisecond)
+	for range ticker.C {
+		select {
+		case req := <-recv:
+			_, ok := who_has[req.info_hash]
+			if ok {
+				req.port <- tracker_response{"", "success, file found", 5, 0, "*_*",
+					who_has[req.info_hash].num_seeders,
+					who_has[req.info_hash].num_leechers,
+					who_has[req.info_hash].peer_list}
+			} else {
+				req.port <- tracker_response{"File not found", "Request denied", 0, 0, "",
+					0, 0, nil}
+			}
+		case <-exit:
+			fmt.Println("Tracker server closing.")
+			return
+		default:
+			fmt.Println("Tracker server standby.")
+		}
+	}
 
 }
-
