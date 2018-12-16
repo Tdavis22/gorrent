8406aefd7754c71a4871153d9fc7977b388c4cf1
anujkak <anujkak@users.noreply.github.com>
2018-12-16 09:55:37 -0800

    client

--- clientMain.go
+++ clientMain.go
@@ -8,11 +8,8 @@ import (
 
 const trackerUrl = "udp://tracker.coppersurfer.tk:6969/announce"
 
-
-
-
 func updMessage() {
-	p :=  make([]byte, 2048)
+	p := make([]byte, 2048)
 	conn, err := net.Dial("udp", "127.0.0.1:1234")
 	if err != nil {
 		fmt.Printf("Some error %v", err)
@@ -26,4 +23,20 @@ func updMessage() {
 		fmt.Printf("Some error %v\n", err)
 	}
 	conn.Close()
+}
+func client_server(filename string, talk_to_tracker chan get_request) []byte {
+	tracker_resp := make(chan tracker_response)
+	talk_to_tracker <- get_request{filename, "0", tracker_resp, 0, 0, 0, true, "started", "", 30, "", ""}
+	t_resp := <-tracker_resp //block waiting for tracker.
+	peers := t_resp.peers
+	peers_resp := make(chan other_message)
+	for i := 0; i < len(peers); i++ {
+		peer_connect := peers[i].port
+		peer_connect <- other_message{i * 512, 2, nil, filename, peers_resp}
+	}
+	f := make([]byte, 2048)
+	for data := range peers_resp {
+		copy(f[data.offset:], data.payload)
+	}
+	return f
 }
